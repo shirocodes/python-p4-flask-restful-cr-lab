de1627bf03ac8dd6cae83cc40c63b58e4df78c12
shirocodes <ishirapt@gmail.com>
2025-06-21 18:31:55 +0300

    lab complete.

--- server/app.py
+++ server/app.py
@@ -17,11 +17,52 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
-
-class PlantByID(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
         
+        response_dict_list = [n.to_dict() for n in plants]
+        
+        return make_response(response_dict_list, 200)
+    
+    def post(self):
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "Invalid JSON"}), 400
+        
+        name = data.get("name")
+        image = data.get("image")
+        price = data.get("price")
+        
+        if not all([name, image, price]):
+            return jsonify({"error": "Missing required fields"}), 400
+        
+        try:
+            new_record = Plant(
+            name=name,
+            image=image,
+            price=float(price)
+            )
+            db.session.add(new_record)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return {"error": str(e)}, 400
+        
+        return new_record.to_dict(), 201
+    
+api.add_resource(Plants, '/plants')
+        
+class PlantByID(Resource):
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        
+        if not plant:
+            return {"error": "not found"}, 400
+        
+        response_dict = plant.to_dict()
+        return make_response(response_dict, 200)
+        
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
